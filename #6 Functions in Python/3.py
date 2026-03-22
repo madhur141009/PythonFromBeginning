08d6d9eeaf9119501a022d8d8ae32dad7e36da97
Madhur Garg <137780454+madhur141009@users.noreply.github.com>
2026-03-22 19:56:39 +0530

    Add files via upload

---
+++ #6 Functions in Python/3.py
@@ -0,0 +1,5 @@
+#How to use a function
+
+def area(length, breadth):
+    area = length * breadth
+    print("The area is ", area)
---
+++ #7 Comments in Python/Example.py
@@ -0,0 +1,20 @@
+#This is a single-line comment
+
+"""
+
+This 
+is a 
+multi-line
+comment
+
+"""
+
+'''
+
+When this script
+is executed
+it shows no output
+because it is full 
+of comments.
+
+'''
